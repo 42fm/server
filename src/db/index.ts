5984cfc167505ab4513c674bdad64d2b833cd6c6
loczek <30776250+loczek@users.noreply.github.com>
2022-08-25 01:59:49 +0200

    refactor database connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,34 +1,21 @@
-import Redis from "ioredis";
 import "dotenv/config";
-import { createConnection } from "typeorm";
-import { log } from "../utils/loggers";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Channel } from "./entity/Channel";
 import { User } from "./entity/User";
 
-const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, REDIS_URL } = process.env;
+const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 
-export const connection = createConnection({
+const connection = new DataSource({
   type: "postgres",
   host: DB_HOST,
   port: 5432,
   username: DB_USERNAME,
   password: DB_PASSWORD,
   database: DB_NAME,
-  entities: [User],
-  synchronize: true,
+  entities: [User, Channel],
+  migrations: ["src/db/migrations/**/*.ts"],
   logging: false,
 });
 
-export const redisClient = new Redis(REDIS_URL);
-export const sub = new Redis(REDIS_URL);
-
-redisClient.on("connect", () => {
-  log.debug("Redis client connected");
-});
-
-sub.on("connect", () => {
-  log.debug("Redis subscriber connected");
-});
-
-sub.psubscribe(["__keyspace@0__:*:current"], () => {
-  log.debug("Redis subscriber subscribed to current keyspace");
-});
+export default connection;
---
+++ src/db/redis.ts
@@ -0,0 +1,19 @@
+import Redis from "ioredis";
+import { log } from "../utils/loggers";
+
+const { REDIS_URL } = process.env;
+
+export const redisClient = new Redis(REDIS_URL);
+export const sub = new Redis(REDIS_URL);
+
+redisClient.on("connect", () => {
+  log.debug("Redis client connected");
+});
+
+sub.on("connect", () => {
+  log.debug("Redis subscriber connected");
+});
+
+sub.psubscribe("__keyspace@0__:*:current", () => {
+  log.debug("Redis subscriber subscribed to current keyspace");
+});
