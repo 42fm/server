c2d15c80c23768c91da2f9e5a1ef1bfa07373978
loczek <30776250+loczek@users.noreply.github.com>
2023-05-21 23:56:15 +0200

    refactor: song adding, responses and join detection

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,35 @@
+import { youtube } from "@googleapis/youtube";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import { createServer } from "http";
+import { parse, toSeconds } from "iso8601-duration";
 import "reflect-metadata";
 import { Server, ServerOptions } from "socket.io";
 import tmi from "tmi.js";
-import health from "./routes/health";
 import ytdl from "ytdl-core";
 import ytsr from "ytsr";
-import { ClientToServerEvents, CurrentSong, InterServerEvents, ServerToClientEvents, SocketData, Song } from "../types";
-import connection from "./db";
 import { User } from "./db/entity/User";
+import connection from "./db/index";
 import { redisClient, sub } from "./db/redis";
 import morganMiddleware from "./middleware/morganMiddleware";
 import auth from "./routes/auth";
+import health from "./routes/health";
 import { songs } from "./songs";
 import { log } from "./utils/loggers";
+import { Responder } from "./utils/reply";
 import { sleep } from "./utils/sleep";
 
-let SONG_MIN_VIEWS = 25_000; // 100k views
+const youtubeApi = youtube({
+  version: "v3",
+});
+
+let SONG_MIN_VIEWS = 25_000; // 25k views
 let SONG_MIN_LENGTH = 60; // 1 minute
 let SONG_MAX_LENGTH = 600; // 10 minutes
 let ONE_HOUR = 3600;
-let SKIP_TOPIC = true;
+let SKIP_TOPIC = false;
 let SONG_COMMAND = false;
 
 const {
@@ -38,13 +46,21 @@ const {
 const app = express();
 const httpServer = createServer(app);
 
+app.use(
+  cors({
+    origin: NODE_ENV === "production" ? "https://42fm.app" : "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(morganMiddleware);
+app.use(cookieParser());
+
 app.use(health);
 app.use(auth);
-app.use(morganMiddleware);
 
 const options: Partial<ServerOptions> = {
   cors: {
-    origin: ["https://test.com"],
+    origin: ["http://localhost:5713"],
   },
 };
 
@@ -106,22 +122,24 @@ client.on("message", async (channel, tags, message, self) => {
   const isMod = tags.mod;
   const isOwner = tags["user-id"] === FM_OWNER_ID;
 
+  const responder = new Responder(client, tags, room);
+
   log.info("Command sent", { username: tags["display-name"], channel, command: command, args });
 
   // Commands for owner only
   if (isOwner) {
     if (command === "ping") {
-      client.say(room, `Pong imGlitch 👍`);
+      responder.respond("Pong imGlitch 👍");
       return;
     }
     if (command === "channels") {
       const channels = client.getChannels().map((channel) => channel.slice(1));
-      client.say(room, `Connected channels: ${channels.join(", ")}`);
+      responder.respond(`Connected channels: ${channels.join(", ")}`);
       return;
     }
     if (command === "count") {
       const count = client.getChannels().length;
-      client.say(room, `Connected channels: ${count}`);
+      responder.respond(`Connected channels: ${count}`);
       return;
     }
     if (command === "random") {
@@ -130,36 +148,36 @@ client.on("message", async (channel, tags, message, self) => {
     if (command === "ws") {
       // return count of connected websockets
       const count = await io.fetchSockets();
-      client.say(room, `Connected ws: ${count.length}`);
+      responder.respond(`Connected ws: ${count.length}`);
       return;
     }
 
     if (command === "set") {
-      if (args[1] === "minviews") {
-        SONG_MIN_VIEWS = Number(args[2]);
-        client.say(room, `Minimum views set to ${SONG_MIN_VIEWS}`);
+      if (args[2] === "minviews") {
+        SONG_MIN_VIEWS = Number(args[3]);
+        responder.respond(`Minimum views set to ${SONG_MIN_VIEWS}`);
         return;
       }
-      if (args[1] === "minlength") {
-        SONG_MIN_LENGTH = Number(args[2]);
-        client.say(room, `Minimum length set to ${SONG_MIN_LENGTH}`);
+      if (args[2] === "minlength") {
+        SONG_MIN_LENGTH = Number(args[3]);
+        responder.respond(`Minimum length set to ${SONG_MIN_LENGTH}`);
         return;
       }
-      if (args[1] === "maxlength") {
-        SONG_MAX_LENGTH = Number(args[2]);
-        client.say(room, `Maximum length set to ${SONG_MAX_LENGTH}`);
+      if (args[2] === "maxlength") {
+        SONG_MAX_LENGTH = Number(args[3]);
+        responder.respond(`Maximum length set to ${SONG_MAX_LENGTH}`);
         return;
       }
     }
     if (command === "toggle") {
-      if (args[1] === "topic") {
+      if (args[2] === "topic") {
         SKIP_TOPIC = !SKIP_TOPIC;
-        client.say(room, `Skip topic: ${SKIP_TOPIC}`);
+        responder.respond(`Skip topic: ${SKIP_TOPIC}`);
         return;
       }
-      if (args[1] === "song") {
+      if (args[2] === "song") {
         SONG_COMMAND = !SONG_COMMAND;
-        client.say(room, `Song command: ${SONG_COMMAND}`);
+        responder.respond(`Song command: ${SONG_COMMAND}`);
         return;
       }
     }
@@ -176,7 +194,7 @@ client.on("message", async (channel, tags, message, self) => {
       ut_min = ut_min % 60;
       ut_sec = ut_sec % 60;
 
-      client.say(room, `Uptime: ${ut_hour}h ${ut_min}m ${ut_sec}s`);
+      responder.respond(`Uptime: ${ut_hour}h ${ut_min}m ${ut_sec}s`);
       return;
     }
   }
@@ -193,17 +211,17 @@ client.on("message", async (channel, tags, message, self) => {
           const [currentError, current] = replies[1] as any;
 
           if (ttlError || currentError) {
-            client.say(room, "Error while pausing");
+            responder.respond("Error while pausing");
             return;
           }
 
           if (ttl === -2) {
-            client.say(room, "Nothing to pause");
+            responder.respond("Nothing to pause");
             return;
           }
 
           if (ttl === -1) {
-            client.say(room, "Song already paused");
+            responder.respond("Song already paused");
             return;
           }
 
@@ -213,7 +231,7 @@ client.on("message", async (channel, tags, message, self) => {
             .set(`${room}:timeRemaining`, ttl)
             .then(() => {
               io.in(room).emit("pause");
-              client.say(room, "Song paused");
+              responder.respond("Song paused");
             })
             .catch((err) => log.error(err));
         });
@@ -229,12 +247,12 @@ client.on("message", async (channel, tags, message, self) => {
           // const [timeRemainingDelError, timeRemainingDel] = replies[2];
 
           if (currentError || timeRemainingError) {
-            client.say(room, "Error while playing");
+            responder.respond("Error while playing");
             return;
           }
 
           if (current === null) {
-            client.say(room, "Nothing to play");
+            responder.respond("Nothing to play");
             return;
           }
 
@@ -245,7 +263,7 @@ client.on("message", async (channel, tags, message, self) => {
             .expire(`${room}:current`, timeRemaining)
             .then(() => {
               io.in(room).emit("play");
-              client.say(room, "Song playing");
+              responder.respond("Song playing");
             })
             .catch((err) => log.error(err));
           // }
@@ -260,43 +278,40 @@ client.on("message", async (channel, tags, message, self) => {
         .del(`${room}:current`)
         .del(`${room}:playlist`)
         .exec(() => {
-          client.say(room, "Playlist cleared");
+          responder.respond("Playlist cleared");
           io.in(room).emit("clear");
         })
         .catch((err) => log.error(err));
       return;
     } else if (command === "help") {
-      client.say(
-        room,
-        `@${tags["display-name"]} Available commands: <link>, help, play, pause, skip, clear, song, disconnect`
-      );
+      responder.respondWithMention(`Available commands: <link>, help, play, pause, skip, clear, song, disconnect`);
       return;
     } else if (command === "disconnect") {
-      client.say(room, `@${tags["display-name"]}, disconnecting... :(`);
+      responder.respondWithMention(`disconnecting... :(`);
       client.part(room);
       return;
     } else if (command === "ban") {
       const user = args[1];
       if (!user) {
-        client.say(room, `@${tags["display-name"]}, please specify a user to ban`);
+        responder.respondWithMention(`please specify a user to ban`);
         return;
       }
-      client.say(room, `@${tags["display-name"]}, ${user} banned`);
+      responder.respondWithMention(`${user} banned`);
       return;
     } else if (command === "unban") {
       const user = args[1];
       if (!user) {
-        client.say(room, `@${tags["display-name"]}, please specify a user to unban`);
+        responder.respondWithMention(`please specify a user to unban`);
         return;
       }
-      client.say(room, `@${tags["display-name"]}, ${user} unbanned`);
+      responder.respondWithMention(`${user} unbanned`);
       return;
     }
   }
 
   // Commands for everyone
   if (command === "help") {
-    client.say(room, `@${tags["display-name"]}, available commands: !fm <link>, !fm search <term>, !fm song, !fm wrong`);
+    responder.respondWithMention(`available commands: !fm <link>, !fm search <term>, !fm song, !fm wrong`);
     return;
   }
 
@@ -311,7 +326,7 @@ client.on("message", async (channel, tags, message, self) => {
 
         const currentSong: CurrentSong = JSON.parse(current);
 
-        client.say(room, `@${tags["display-name"]}, currently playing: "${currentSong.url}"`);
+        responder.respondWithMention(`currently playing: "${currentSong.url}"`);
       })
       .catch((err) => log.error(err));
     return;
@@ -322,7 +337,7 @@ client.on("message", async (channel, tags, message, self) => {
     const list = current.map((item) => JSON.parse(item));
 
     if (list.length < 1) {
-      client.say(room, `@${tags["display-name"]}, playlist empty`);
+      responder.respondWithMention(`playlist empty`);
       return;
     }
 
@@ -336,13 +351,13 @@ client.on("message", async (channel, tags, message, self) => {
     }
 
     if (!found) {
-      client.say(room, `@${tags["display-name"]}, could not find your last added song`);
+      responder.respondWithMention(`could not find your last added song`);
       return;
     }
 
     redisClient.lrem(`${room}:playlist`, -1, JSON.stringify(found));
 
-    client.say(room, `@${tags["display-name"]}, removed your last song`);
+    responder.respondWithMention(`removed your last song`);
     return;
   }
 
@@ -350,22 +365,31 @@ client.on("message", async (channel, tags, message, self) => {
     const [_one, _two, ...search] = args;
 
     if (!search.length) {
-      client.say(room, `@${tags["display-name"]}, no search query provided`);
+      responder.respondWithMention(`no search query provided`);
       return;
     }
 
     try {
       const searchResults = await ytsr(search.join(" "));
       const vid = searchResults.items.find((item) => item.type === "video");
-      if (vid.type === "video") {
+      if (vid!.type === "video") {
         log.info("SEARCH", { vid });
         command = vid.url;
       }
     } catch {
-      client.say(room, `@${tags["display-name"]}, no search query provided`);
+      responder.respondWithMention(`no search query provided`);
       return;
     }
   }
+
+  // if (command === "spotify") {
+  //   const [_one, _two, ...spotifyUrl] = args;
+
+  //   responder.respondWithMention(`test`);
+
+  //   console.log(spotifyUrl);
+  //   return;
+  // }
 
   // Check if yt url is not valid
   const isNotValid = !ytdl.validateURL(command);
@@ -399,57 +423,75 @@ client.on("message", async (channel, tags, message, self) => {
         const id = ytdl.getVideoID(url);
 
         try {
-          let info = await ytdl.getInfo(url);
-
-          if (!(isBroadcaster || isMod || isOwner)) {
-            if (Number(info.videoDetails.viewCount) < SONG_MIN_VIEWS) {
-              log.info("Not enough views");
-              client.say(room, `@${username}, not enough views`);
-              return;
-            }
-            if (Number(info.videoDetails.lengthSeconds) < SONG_MIN_LENGTH) {
-              log.info("Not long enough");
-              client.say(room, `@${username}, song not long enough`);
-              return;
-            }
-            if (Number(info.videoDetails.lengthSeconds) > SONG_MAX_LENGTH) {
-              log.info("Too long");
-              client.say(room, `@${username}, song too long`);
-              return;
-            }
-          }
-
-          if (SKIP_TOPIC) {
-            if (info.videoDetails.author.name.toLowerCase().includes("topic")) {
-              log.info("Topic song skipping");
-              client.say(room, `@${username}, song type topic is not allowed try another link`);
-              return;
-            }
-          }
-
-          log.info(info.videoDetails.viewCount);
-
-          let audioFormats = ytdl.filterFormats(info.formats, "videoandaudio");
-          const format = ytdl.chooseFormat(audioFormats, {
-            quality: "highest",
+          const response = await youtubeApi.videos.list({
+            part: ["contentDetails", "status", "snippet", "statistics"],
+            id: [id],
+            key: process.env.GOOGLE_API_KEY,
           });
 
-          let song: Song = {
-            yt_id: id,
-            title: info.videoDetails.title,
-            // title: info.videoDetails.media.song || info.videoDetails.title,
-            artist: info.videoDetails.author.name,
-            // artist: info.videoDetails.media.artist || info.videoDetails.author.name,
-            url: format?.url,
-            imgUrl: info.videoDetails.author.thumbnails[info.videoDetails.author.thumbnails.length - 1].url,
-            duration: Number(info.videoDetails.lengthSeconds),
-          };
+          let item = response.data.items![0];
+
+          let isEmbeddable = item.status!.embeddable;
+          let isAgeRestricted = item.contentDetails!.contentRating!.ytRating === "ytAgeRestricted";
+
+          let title = item.snippet!.title!;
+          let channelName = item.snippet!.channelTitle!;
+          let views = Number(item.statistics!.viewCount);
+          let duration = toSeconds(parse(item.contentDetails!.duration!));
+
+          if (!isEmbeddable) {
+            responder.respondWithMention("video has embedds disabled");
+            return;
+          }
+
+          if (isAgeRestricted) {
+            responder.respondWithMention("video is age restricted");
+            return;
+          }
+
+          if (!(isBroadcaster || isMod || isOwner)) {
+            if (views < SONG_MIN_VIEWS) {
+              log.info("Not enough views");
+              responder.respondWithMention("song doesn't have enough views");
+              return;
+            }
+            if (duration < SONG_MIN_LENGTH) {
+              log.info("Not long enough");
+              responder.respondWithMention("song too short");
+              return;
+            }
+            if (duration > SONG_MAX_LENGTH) {
+              log.info("Too long");
+              responder.respondWithMention("song too long");
+              return;
+            }
+          }
+
+          // // let audioFormats = ytdl.filterFormats(info.formats, "videoandaudio");
+          // // const format = ytdl.chooseFormat(audioFormats, {
+          // //   quality: "highest",
+          // // });
+
+          // let song: Song = {
+          //   yt_id: id,
+          //   title: info.videoDetails.title,
+          //   artist: info.videoDetails.author.name,
+          //   url: format?.url ?? "xd",
+          //   imgUrl: info.videoDetails.author.thumbnails[info.videoDetails.author.thumbnails.length - 1].url,
+          //   duration: Number(info.videoDetails.lengthSeconds),
+          // };
 
           // Cache the song
           // redisClient.setex(id, SONG_EXPIRATION, JSON.stringify(song));
 
-          song = {
-            ...song,
+          let song: Song = {
+            yt_id: id,
+            title,
+            artist: channelName,
+            url: "https://youtube.com/",
+            imgUrl:
+              "https://static-cdn.jtvnw.net/jtv_user_pictures/c8f064a7-364f-460c-b668-75beb734e3aa-profile_image-70x70.png",
+            duration,
             username,
           };
 
@@ -550,17 +592,38 @@ async function connectToChannels() {
 }
 
 async function main() {
+  // try {
+  //   await redisClient.connect();
+  //   await connection.initialize();
+  // } catch (e) {
+  //   log.info("Unable to connect to some db");
+  //   log.error(e);
+  // }
+
   try {
     await client.connect();
     await connectToChannels();
   } catch (e) {
-    log.info("Eror in client conection");
+    log.info("Error in client conection");
     log.error(e);
   }
 
-  io.on("connection", (socket) => {
-    socket.handshake;
+  if (process.env.NODE_ENV === "development") {
+    let user = await User.findOne({ where: { twitch_id: "36768120" } });
 
+    if (!user) {
+      let newUser = User.create({
+        twitch_id: "36768120",
+        username: "loczuk",
+        display_name: "Loczuk",
+        email: "test@test.com",
+      });
+
+      await newUser.save();
+    }
+  }
+
+  io.on("connection", (socket) => {
     log.info("New connection", { id: socket.id, ip: socket.handshake.address });
 
     socket.on("error", (err) => {
@@ -574,13 +637,14 @@ async function main() {
       //   return;
       // }
 
-      // const user = await User.findOne({ where: { username: data.room } });
+      const user = await User.findOne({ where: { username: data.room } });
 
-      // if (!user) {
-      //   log.info("Channel does not exist", { channel: data.room });
-      //   socket.emit("no42fm");
-      //   return;
-      // }
+      if (!user) {
+        log.info("42fm not enabled on channel", { channel: data.room });
+        socket.emit("no42fm");
+        socket.disconnect();
+        return;
+      }
 
       // if (!user.channel.isEnabled) {
       //   log.info(`Channel is not enabled`, { channel: data.room });
@@ -589,14 +653,14 @@ async function main() {
       //   return;
       // }
 
-      const is42fm = client.getChannels().includes(`#${data.room}`);
+      // const is42fm = client.getChannels().includes(`#${data.room}`);
 
-      if (!is42fm) {
-        log.info(`Channel is not enabled`, { channel: data.room });
-        socket.emit("no42fm");
-        socket.disconnect();
-        return;
-      }
+      // if (!is42fm) {
+      //   log.info(`Channel is not enabled`, { channel: data.room });
+      //   socket.emit("no42fm");
+      //   socket.disconnect();
+      //   return;
+      // }
 
       log.info("Socket joined room", { socket: socket.id, room: data.room });
       await socket.join(data.room);
@@ -613,7 +677,7 @@ async function main() {
         .exec((err, replies) => {
           const [currentError, current] = replies[0] as any;
           const [playlistError, playlist] = replies[1] as any;
-          const [ttlError, ttl] = replies[2];
+          const [ttlError, ttl] = replies[2] as any;
 
           let isPlaying;
 
