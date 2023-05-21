07579cd5846efd965a33da1e0c365715244e0e10
loczek <30776250+loczek@users.noreply.github.com>
2023-05-21 23:36:04 +0200

    update redis import

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,10 +1,10 @@
-import Redis from "ioredis";
+import { Redis } from "ioredis";
 import { log } from "../utils/loggers";
 
 const { REDIS_URL } = process.env;
 
-export const redisClient = new Redis(REDIS_URL);
-export const sub = new Redis(REDIS_URL);
+export const redisClient = new Redis(REDIS_URL!);
+export const sub = new Redis(REDIS_URL!);
 
 redisClient.on("connect", () => {
   log.debug("Redis client connected");
