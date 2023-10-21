c42258f146ee8f6134407d06d024a6fdc2f08001
loczek <30776250+loczek@users.noreply.github.com>
2023-10-21 11:23:26 +0200

    fix: remove duplicate middleware

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,11 +1,8 @@
 import { Router } from "express";
-import morganMiddleware from "../middleware/morganMiddleware";
 
 const router = Router();
 
-router.use(morganMiddleware);
-
-router.get("/health", async (req, res) => {
+router.get("/health", (req, res) => {
   res.sendStatus(200);
 });
 
