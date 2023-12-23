b2c1f5bb90e212716a943cd92f219b459a595658
loczek <30776250+loczek@users.noreply.github.com>
2023-12-23 03:05:15 +0100

    fix: remove unnecessary brackets

--- src/utils/loggers.ts
+++ src/utils/loggers.ts
@@ -1,4 +1,3 @@
-import { TransformableInfo } from "logform";
 import winston, { createLogger, format, transports } from "winston";
 
 let logger: winston.Logger;
@@ -7,6 +6,8 @@ if (process.env.NODE_ENV === "development") {
   logger = createLogger({
     level: "silly",
     format: format.combine(
+      format.align(),
+      format.metadata(),
       format.colorize(),
       format.timestamp({
         format: "HH:mm:ss",
@@ -14,7 +15,10 @@ if (process.env.NODE_ENV === "development") {
       format.prettyPrint(),
       format.errors({ stack: true }),
       format.splat(),
-      format.printf((info) => `[${info.timestamp}][${info.level}]: ${info.message} ${rest(info)}`)
+      format.printf(
+        (info) =>
+          `${info.timestamp} [${info.level}] ${info.message} ${!isEmpty(info.metadata) ? JSON.stringify(info.metadata) : ""}`
+      )
     ),
     transports: [new transports.Console()],
   });
@@ -24,23 +28,10 @@ if (process.env.NODE_ENV === "development") {
     format: format.combine(format.prettyPrint(), format.errors({ stack: true }), format.splat(), format.simple()),
     transports: [new transports.Console()],
   });
-  // log = createLogger({
-  //   level: "warn",
-  //   format: format.combine(format.json(), format.errors({ stack: true }), format.splat()),
-  //   transports: [new transports.Console()],
-  // });
 }
 
 export { logger };
 
-function rest(info: TransformableInfo) {
-  return JSON.stringify(
-    Object.assign({}, info, {
-      level: undefined,
-      message: undefined,
-      splat: undefined,
-      label: undefined,
-      timestamp: undefined,
-    })
-  );
+function isEmpty(obj: {}) {
+  return Object.keys(obj).length === 0;
 }
