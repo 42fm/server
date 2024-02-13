9719d10b47985f1822ba1f1f75834c9c7fb5be7b
loczek <30776250+loczek@users.noreply.github.com>
2024-02-13 05:10:13 +0100

    lower min views and length

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,8 +1,8 @@
 import { Config } from "@lib/config";
 
 const config = new Config({
-  SONG_MIN_VIEWS: 10_000,
-  SONG_MIN_LENGTH: 60,
+  SONG_MIN_VIEWS: 5_000,
+  SONG_MIN_LENGTH: 30,
   SONG_MAX_LENGTH: 1_200,
 });
 
