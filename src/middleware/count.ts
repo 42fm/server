99484ccf7d4bc4a5f6b147fa13c269ee0a5c6b9a
loczek <30776250+loczek@users.noreply.github.com>
2024-04-15 18:14:32 +0200

    fix: bot not responding

--- src/middleware/count.ts
+++ src/middleware/count.ts
@@ -1,13 +1,7 @@
 import { redisClient } from "@db/redis";
 import { Context } from "@lib/router";
-import { logger } from "@utils/loggers";
 
 export async function countUsage(ctx: Context, args: string[], next: () => void) {
-  try {
-    await redisClient.incr(`count:${ctx.room}`);
-  } catch (e) {
-    logger.error(e);
-  }
-
+  redisClient.incr(`count:${ctx.room}`);
   next();
 }
