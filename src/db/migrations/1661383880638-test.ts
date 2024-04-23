2168a29e38d006503c6d47e75da661cd58efbe12
loczek <30776250+loczek@users.noreply.github.com>
2024-04-23 19:08:50 +0200

    fix: create user table

--- src/db/migrations/1661383880638-test.ts
+++ src/db/migrations/1661383880638-test.ts
@@ -1,21 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class test1661383880638 implements MigrationInterface {
-    name = 'test1661383880638'
+export class Init1661383880638 implements MigrationInterface {
+    name = 'Init1661383880638'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "channel" ("id" SERIAL NOT NULL, "isEnabled" boolean NOT NULL DEFAULT true, CONSTRAINT "PK_590f33ee6ee7d76437acf362e39" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "channelId" SERIAL NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "PK_cace4a159ff9f2512dd42373760"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "PK_fe52230f2c3184ac491eb94038a" PRIMARY KEY ("id", "channelId")`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "channelIsenabled" boolean NOT NULL DEFAULT true`);
+        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "twitch_id" character varying NOT NULL, "username" character varying NOT NULL, "display_name" character varying NOT NULL, "email" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "channelId" SERIAL NOT NULL, "channelIsenabled" boolean NOT NULL DEFAULT true, CONSTRAINT "PK_fe52230f2c3184ac491eb94038a" PRIMARY KEY ("id", "channelId"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "channelIsenabled"`);
-        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "PK_fe52230f2c3184ac491eb94038a"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")`);
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "channelId"`);
+        await queryRunner.query(`DROP TABLE "user"`);
         await queryRunner.query(`DROP TABLE "channel"`);
     }
 
