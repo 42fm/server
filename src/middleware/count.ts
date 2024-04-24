85ec4e66018bfbea42f7149422bf2b470d884ebd
loczek <30776250+loczek@users.noreply.github.com>
2024-04-24 23:58:13 +0200

    remove count middleware

--- src/middleware/count.ts
+++
@@ -1,7 +0,0 @@
-import { redisClient } from "@db/redis.js";
-import { Context } from "@lib/router.js";
-
-export async function countUsage(ctx: Context, args: string[], next: () => void) {
-  redisClient.incr(`count:${ctx.room}`);
-  next();
-}
