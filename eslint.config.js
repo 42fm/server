8b448c12ce7278c67cdd4219a3273e8549932334
loczek <30776250+loczek@users.noreply.github.com>
2024-04-27 23:30:46 +0200

    migrate to eslint v9

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,17 @@
-{
-  "env": {
-    "es2021": true,
-    "node": true
+import pluginJs from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+/**
+ * @type {import('eslint').Linter.FlatConfig[]}
+ */
+export default [
+  { languageOptions: { globals: globals.nodeBuiltin } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
   },
-  "extends": ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
-  },
-  "plugins": ["@typescript-eslint"]
-}
+];
