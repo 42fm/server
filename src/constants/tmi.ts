c5b45da111fc33018dbd9eecabfeb830d49057f7
loczek <30776250+loczek@users.noreply.github.com>
2025-03-11 17:57:45 +0100

    feat: add logger to tmi

--- src/constants/tmi.ts
+++ src/constants/tmi.ts
@@ -1,6 +1,11 @@
 import tmi from "#tmi";
+import { logger } from "@utils/loggers.js";
 
 const { NODE_ENV, DEBUG_TMI, TWITCH_USERNAME, TWITCH_OAUTH } = process.env;
+
+const clientLogger = logger.child({
+  service: "tmi",
+});
 
 export const client = new tmi.Client({
   options: {
@@ -10,6 +15,11 @@ export const client = new tmi.Client({
   connection: {
     reconnect: NODE_ENV === "production",
     secure: NODE_ENV === "production",
+  },
+  logger: {
+    info: (msg) => clientLogger.info(msg),
+    warn: (msg) => clientLogger.warn(msg),
+    error: (msg) => clientLogger.error(msg),
   },
   identity: {
     username: TWITCH_USERNAME,
