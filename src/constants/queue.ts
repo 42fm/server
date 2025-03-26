4ff7b79a6f675d2bd6352e188febe7ff497e6ab1
loczek <30776250+loczek@users.noreply.github.com>
2025-03-26 12:50:36 +0100

    fix: set correct rate limit

--- src/constants/queue.ts
+++ src/constants/queue.ts
@@ -2,7 +2,8 @@ import { Queue } from "@lib/queue.js";
 
 const { NODE_ENV } = process.env;
 
+// https://dev.twitch.tv/docs/chat/#rate-limits
 export const queue = new Queue({
-  limitPerInterval: NODE_ENV === "production" ? 30 : 5,
+  limitPerInterval: NODE_ENV === "production" ? 20 : 5,
   reseltLimitMs: 30_000,
 });
